6411c623a5ea220c66b89cd8dd2cd28aa637c3d5
doug-pires <dpiresmartins@yahoo.com.br>
2024-10-12 13:31:22 +0100

    add example using array

---
+++ arrays.py
@@ -0,0 +1,22 @@
+"""
+
+Array == List
+
+[]
+[1, 2, 3, 4, 5]
+[1, 2, 3, 4, 5, 'a', 'b', 'c', 'd', 'e']
+
+"""
+
+array_of_integers = [1, 2, 3, 4, 5]
+
+
+# Append an element to the end of the list
+
+array_of_integers.append(6)
+array_of_integers.append(7)
+print(array_of_integers)
+
+
+array_of_integers.sort(reverse=True)
+print(array_of_integers)
