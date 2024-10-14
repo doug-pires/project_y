ca3b2c7ca738a7e9e2abfb73b0b5a69f6ad4d2ab
doug-pires <dpiresmartins@yahoo.com.br>
2024-10-14 14:02:54 +0100

    add pendulum library

--- libraries.py
+++ libraries.py
@@ -1,4 +1,12 @@
 from datetime import datetime, timedelta, timezone
 
+# Using the library pendulum
+import pendulum
+
 print("Portuguese time", datetime.now())
 print("German time", datetime.now(timezone(timedelta(hours=2))))
+
+
+print("Portuguese time", pendulum.now("Europe/Lisbon"))
+print("German time", pendulum.now("Europe/Berlin"))
+print("Sao Paulo time", pendulum.now("America/Sao_Paulo"))
